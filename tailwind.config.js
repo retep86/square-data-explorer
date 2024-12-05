734d6fa0dcb866493598fc8fc857ef982e5eb790
retep86 <petervigilante@hotmail.com>
2024-12-06 10:05:55 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ['0.8rem', '1.2rem'], // Small text reduced
+        base: ['0.9rem', '1.4rem'], // Base text reduced
+        lg: ['1rem', '1.6rem'], // Large text reduced
+        xl: ['1.125rem', '1.75rem'], // Extra-large text reduced
+        '2xl': ['1.25rem', '1.75rem'], // 2xl reduced
+        // Add more overrides as needed
+      },
+    },
   },
   plugins: [],
 }
